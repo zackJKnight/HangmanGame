eb531d341e32d111c620387493e6c3889f30d423
zackJKnight <zachary.j.knight@gmail.com>
2017-08-01 22:48:36 -0500

    Attempt to use build agent module path.

--- HangmanGame.Tests.ps1
+++ HangmanGame.Tests.ps1
@@ -1,14 +1,18 @@
-
+<#
 if(-not (Test-Path $pshome/Modules/HangmanGame)){
     New-Item "$pshome/Modules/HangmanGame" -ItemType Directory
 }
-Copy-Item ./HangmanGame.psm1 $pshome/Modules/HangmanGame -Force -Verbose
-Import-Module $pshome/Modules/HangmanGame/HangmanGame.psm1 -Force -Verbose
-
+#>
+$AgentModulePath = "$($env:AGENT_HOMEDIRECTORY)\agent\worker\Modules"
+New-Item $AgentModulePath/HangmanGame -ItemType Directory -Force
+Copy-Item ./HangmanGame.psm1 $AgentModulePath -Force -Verbose
+#Copy-Item ./HangmanGame.psm1 $pshome/Modules/HangmanGame -Force -Verbose
+#Import-Module $pshome/Modules/HangmanGame/HangmanGame.psm1 -Force -Verbose
+Import-Module $AgentModulePath/HangmanGame/HangmanGame.psm1 -Force -Verbose
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', '.psm1'
 . "$here/$sut"
-Import-Module .\HangmanGame.psm1
+Import-Module $AgentModulePath\HangmanGame.psm1
 #$dat = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Data\.', '.'
 #. "$here\$dat"
 
